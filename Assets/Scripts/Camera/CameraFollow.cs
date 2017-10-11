601702d93befb413bb6625b151f54efc26142f14
vmchawla <vmchawla158kb@hotmail.com>
2017-10-11 16:19:37 +0530

    Initial commit

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothing = 5f;
+
+
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        offset = transform.position - target.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
